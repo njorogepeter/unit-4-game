48717fd7f494376c098d20f00920a8ccf41e4b4b
njorogepeter <pgakonde@yahoo.com>
2019-01-10 21:24:20 -0800

    javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// initialize game variables
+// display random number between 19 -120
+// generate and hide new value for each crystal between 1 - 12
+// capture click for crystal
+// update the player's score counter
+// player wins if total score === random number from beginning of the game
+// player loses if score goes above the random number
+// restart game whenever the player wins or loses
+// keep track of number of games player wins or loses
