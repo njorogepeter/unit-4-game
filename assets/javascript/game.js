25ae135cc6bab206733ea8655fbba36dd0a285a5
njorogepeter <pgakonde@yahoo.com>
2019-01-11 14:25:56 -0800

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,14 @@ var random = Math.floor(Math.random()* 101 +19) //  random number between 19 -12
 
 var wins = 0;
 var losses = 0;
-$(".wins").text(wins);
-$(".losses").text("Losses: " + losses);
+var total = 0;
+$(".userWins").text("Wins: " + wins);
+
+$(".userLosses").text("Losses: " + losses);
+
+
+
+
 // display random number between 19 -120
 $(".matchScore").text(random);
 console.log(random);
@@ -25,25 +31,66 @@ function crystalNum(){
     gem4 = Math.floor(Math.random() * 11 + 1);
 
 }
-// capture click for crystal
-$("#gem1").on("click",function(){
+crystalNum();
 
+// capture click for each crystals
+$("#gem1").on("click",function(){
+    gemOnClick(gem1);
+    console.log(gem1);
 });
 
 $("#gem2").on("click",function(){
-
+    gemOnClick(gem2);
+    console.log(gem2);
 });
 
 $("#gem3").on("click",function(){
-
+    gemOnClick(gem3);
+    console.log(gem3);
 });
+
 $("#gem4").on("click",function(){
-
+    gemOnClick(gem4);
+    console.log(gem4);
 });
-// update the player's score counter
-// player wins if total score === random number from beginning of the game
-// player loses if score goes above the random number
-// restart game whenever the player wins or loses
-// keep track of number of games player wins or loses
 
+// update the player's score counter
+function gemOnClick(gemValue){
+    total = total + gemValue;
+    $("#total").text(total);
+
+// player wins if total score === random number from beginning of the game
+    if(total === random){
+        win();
+        $(".userWins").text("wins:" + wins);
+
+    }
+// player loses if score goes above the random number
+    else if (total > random){
+        loss();
+        $(".userLosses").text("Losses: " + losses);
+
+    }
+}
+// restart game whenever the player wins or loses
+function restart(){
+    random = Math.floor(Math.random()* 101 +19);
+    $(".matchScore").text(random);
+    crystalNum();
+    total = 0;
+    $("#total").text(total);
+}
+// keep track of number of games player wins or loses
+function win(){
+    wins++;
+    // $("wins").text("Wins: " + wins);
+    console.log("winning", wins);
+    restart();
+}
+function loss(){
+    losses++;
+    // $("losses").text("Losses: " + losses);
+    console.log("lossing",losses)
+    restart();
+}
 });
